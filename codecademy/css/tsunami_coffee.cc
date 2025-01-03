65a20b883cac8b9bac23eba4b7a70f9fde9af3f9
Dewi <106343438+ieith@users.noreply.github.com>
2025-01-03 09:24:55 +0000

    Update and rename tsunami_coffee to codecademy/css/tsunami_coffee.cc

--- codecademy/css/tsunami_coffee.cc
+++ codecademy/css/tsunami_coffee.cc
@@ -1,10 +1,4 @@
-Throughout this project, you’ll edit the existing Tsunami Coffee website code so that the website appears correctly on varying screen sizes. In addition, you’ll contribute styles that will make this website more visually appealing. 
-  Skip to Content
-My Home
-Tsunami Coffee
-Project Brief
-Objective
-Improved Styling with CSS
+/*
 Tsunami Coffee
 
 Use your knowledge of relative units and responsive web design to help Tsunami Coffee make their website come to life.
@@ -126,7 +120,7 @@ HTML
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
 CSS
----
+---*/
 html,
 body {
   font-family: Helvetica, sans-serif;
